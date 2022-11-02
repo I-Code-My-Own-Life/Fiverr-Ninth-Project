c9beeef195bc4d1e56368e52a6121840762b75c9
Eman <emanamir546@gmail.com>
2022-11-02 23:42:10 +0500

    Added movement functionality and some other things.

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ let walls = [];
 // The `+` sign indicates a pellet : 
 // This here `-` indicates blank space : 
 // `M` indicates our Player here :
+// The `p` indicates the freezing power :
 // And lastly `E` indicates our Enemy character : 
 function randomIntFromInterval(min,max)
 {
@@ -47,11 +48,27 @@ function setup(){
 }
 
 function draw(){
-    background("white");
+    background("black");
     drawMap();
-    // console.log(frameRate());
+    player.spawn();
+    for(let i = 0; i < walls.length; i++){
+        walls[i].stand();
+    }
+    // console.log(player.x)
 }
 
+class Wall{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.width = TileWidth;
+        this.height = TileHeight;
+    }
+    stand(){
+        fill(139,69,19);
+        rect(this.x,this.y,this.width,this.height);
+    }
+}
 class Player{
     constructor(x,y,width,height,dx,dy){
         this.x = x;
@@ -62,7 +79,10 @@ class Player{
         this.dy = dy;
     }
     spawn(){
-
+        fill("blue");
+        rect(this.x,this.y,this.width,this.height);
+        this.x += this.dx;
+        this.y += this.dy;
     }
 }
 
@@ -81,9 +101,13 @@ class Enemy{
 }
 let pelletWidth = TileWidth/2;
 let pelletHeight = TileHeight/2;
-let pelletIncrement = 10
+let pelletIncrement = 10;
+let player;
+let PLAYER_SPEED = 1.2;
+let i = 0;
+let w = 0;
 function drawMap(){
-    pelletIncrement += 0.1
+    // pelletIncrement += 0.1
     for(let row = 0; row < mapRows; row++){
         for(let col = 0; col < mapColumns; col++){
             let index = row * mapRows + col;
@@ -91,8 +115,8 @@ function drawMap(){
             if(map[index] == "+"){
                 fill("yellow");
                 strokeWeight(1);
-                pelletHeight = (sin(pelletIncrement) * 4) + 4 
-                pelletWidth = (cos(pelletIncrement) * 4) + 7 
+                // pelletHeight = (sin(pelletIncrement) * 4) + 4 
+                // pelletWidth = (cos(pelletIncrement) * 4) + 7 
                 ellipse((TileWidth * col) + 10,(TileHeight * row) + 10,pelletWidth,pelletHeight);
             }
             // Enemy : 
@@ -102,20 +126,65 @@ function drawMap(){
             }
             // Player : 
             else if(map[index] == "M"){
-                fill("black");
-                rect(TileWidth * col,TileHeight * row,TileWidth,TileHeight);
+                if(i <= 0){
+                    player = new Player(TileWidth * col,TileHeight * row,TileWidth,TileHeight,0,0);
+                }
+                i++;
             }
             // Walls : 
-            else if(map[index] == "w"){
-                fill(139,69,19);
-                rect(TileWidth * col,TileHeight * row,TileWidth,TileHeight);
+            else if(map[index] == "w" && w <= 0){
+                console.log("map")
+                walls.push(new Wall(TileWidth * col,TileHeight * row));
             }
             // Freeze Power : 
             else if(map[index] == "p"){
                 fill(143, 235, 204);
                 ellipse((TileWidth * col) + 10,(TileHeight * row) + 10,pelletWidth,pelletHeight);
             }
-            console.log(frameRate())
+            console.log(pelletWidth,pelletHeight)
         }
     }
+    w++;
 }
+function keyboardControl(){
+    addEventListener("keydown",(event)=>{
+        if(event.key == "w" || event.key == "W"){
+            console.log(event.key);
+            player.dy = -PLAYER_SPEED
+        }
+        else if(event.key == "a" || event.key == "A"){
+            player.dx = -PLAYER_SPEED;
+            console.log(event.key);
+        }
+        else if(event.key == "d" || event.key == "D"){
+            player.dx = PLAYER_SPEED;
+            console.log(event.key);
+            
+        }
+        else if(event.key == "s" || event.key == "S"){
+            player.dy = PLAYER_SPEED
+            console.log(event.key);
+        }
+    })
+    addEventListener("keyup",(event)=>{
+        if(event.key == "w" || event.key == "W"){
+            player.dy = 0;   
+            console.log(event.key);
+        }
+        else if(event.key == "a" || event.key == "A"){
+            player.dx = 0;
+            console.log(event.key);
+        }
+        else if(event.key == "d" || event.key == "D"){
+            player.dx = 0;
+            console.log(event.key);
+            
+        }
+        else if(event.key == "s" || event.key == "S"){
+            player.dy = 0;    
+            console.log(event.key);
+        }
+    })
+}
+
+keyboardControl();
